b4b6c94a01b454149778f7951abc13ad2c64c9c5
adayllmaz <yilmaz.ada@ibu.edu.mk>
2026-03-06 19:31:08 +0000

    Finished recursion

--- src/recursion.c
+++ src/recursion.c
@@ -18,3 +18,38 @@ Rules:
 - You cannot use a loop to print rows.
 - Base case must be correct.
 */
+#include <stdio.h>
+
+void print_row(int n);
+void print_pyramid(int n);
+
+int main(void)
+{
+    int n;
+
+    printf("Height: ");
+    scanf("%d", &n);
+
+    print_pyramid(n);
+}
+
+void print_pyramid(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    print_pyramid(n - 1);
+    print_row(n);
+}
+
+void print_row(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+
+    printf("\n");
+}
